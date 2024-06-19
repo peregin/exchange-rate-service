2af354038799e7b2271d7a9ad369e3a9751df242
levi <velocorner.com@gmail.com>
2024-06-20 00:04:23 +0200

    add CORS support

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod route;
 mod service;
 
 use actix_web::{App, HttpServer};
+use actix_cors::Cors;
 use log::info;
 
 #[actix_web::main]
@@ -10,7 +11,15 @@ async fn main() -> std::io::Result<()> {
     let port = option_env!("SERVICE_PORT").unwrap_or("9012");
     info!("starting exchange service on port {port} ...");
 
-    HttpServer::new(|| App::new().configure(route::route::init_routes))
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000/")
+            .allowed_methods(vec!["GET"])
+            .allowed_header(actix_web::http::header::ACCEPT)
+            .allowed_header(actix_web::http::header::CONTENT_TYPE)
+            .max_age(3600);
+        App::new().wrap(cors).configure(route::route::init_routes)
+    })
         .bind(format!("0.0.0.0:{port}"))?
         .run()
         .await
