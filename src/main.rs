e6745dbd4d60bea530485023acdd15af8322f190
levi <velocorner.com@gmail.com>
2024-06-21 23:15:40 +0200

    add CORS host origins

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod service;
 use actix_web::{App, HttpServer};
 use actix_cors::Cors;
 use log::info;
+use regex::Regex;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -12,8 +13,12 @@ async fn main() -> std::io::Result<()> {
     info!("starting exchange service on port {port} ...");
 
     HttpServer::new(|| {
+        let allowed_origins = Regex::new(r".*(localhost|peregin\.com|velocorner\.com)").unwrap();
         let cors = Cors::default()
             .allowed_origin("http://localhost:3000/")
+            .allowed_origin_fn(move |origin, _head| {
+                allowed_origins.is_match(origin.to_str().unwrap())
+            })
             .allowed_methods(vec!["GET"])
             .allowed_header(actix_web::http::header::ACCEPT)
             .allowed_header(actix_web::http::header::CONTENT_TYPE)
