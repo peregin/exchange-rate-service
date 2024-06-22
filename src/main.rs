93fbea660fcefe6fee82e2a74c26c3d4911165c4
levi <velocorner.com@gmail.com>
2024-06-22 09:17:18 +0200

    add CORS host origins - 3

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ mod service;
 
 use actix_web::{App, HttpServer};
 use actix_cors::Cors;
-use log::info;
+use log::{debug, info};
 use regex::Regex;
+use actix_web::http::header;
+
+const ALLOWED_ORIGINS: &str = r".*(localhost|peregin\.com|velocorner\.com)";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -13,22 +16,47 @@ async fn main() -> std::io::Result<()> {
     info!("starting exchange service on port {port} ...");
 
     HttpServer::new(|| {
-        let allowed_origins = Regex::new(r".*(localhost|peregin\.com|velocorner\.com)").unwrap();
+        let origins_regex = Regex::new(ALLOWED_ORIGINS).unwrap();
         let cors = Cors::default()
             .allowed_origin_fn(move |origin_header, _request_head| {
                 let origin = origin_header.to_str().unwrap();
-                info!("origin: {origin}");
-                allowed_origins.is_match(origin)
+                debug!("origin: {origin}");
+                is_allowed_origin(origin, &origins_regex)
             })
             .allowed_methods(vec!["GET"])
+            .allowed_headers(&[header::AUTHORIZATION, header::ACCEPT])
             .allowed_headers(vec![
-                actix_web::http::header::ACCEPT,
-                actix_web::http::header::CONTENT_TYPE
+                header::ACCEPT,
+                header::CONTENT_TYPE
             ])
-            .max_age(3600);
+            .block_on_origin_mismatch(false) // allow cURL/HTTPie from working without providing Origin headers
+            .max_age(3600); // preflight cache TTL
         App::new().wrap(cors).configure(route::route::init_routes)
     })
         .bind(format!("0.0.0.0:{port}"))?
         .run()
         .await
+}
+
+fn is_allowed_origin(origin: &str, origins_regex: &Regex) -> bool {
+    origins_regex.is_match(origin)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_allowed_origin() {
+        let origins_regex = Regex::new(ALLOWED_ORIGINS).unwrap();
+
+        // Test allowed origins
+        assert!(is_allowed_origin("https://www.peregin.com", &origins_regex));
+        assert!(is_allowed_origin("https://rates.velocorner.com", &origins_regex));
+        assert!(is_allowed_origin("http://localhost:8000", &origins_regex));
+
+        // Test disallowed origins
+        assert!(!is_allowed_origin("https://www.example.org", &origins_regex));
+        assert!(!is_allowed_origin("http://127.0.0.1", &origins_regex));
+    }
 }
