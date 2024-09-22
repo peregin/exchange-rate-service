f6c15aa9482059bead8eefe17a61c25ec03fd381
levi <velocorner.com@gmail.com>
2024-09-22 14:23:34 +0200

    add multi platform build

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,18 @@
 set -e
 
 # docker build triggers a production install with cargo
-docker build -t peregin/velocorner.rates .
+
+# x86_64
+########
+#docker build -t peregin/velocorner.rates .
+#docker push peregin/velocorner.rates:latest
+
+# aarch64
+#########
+docker buildx build --platform linux/amd64,linux/arm64 -t peregin/velocorner.rates:latest --push .
 
 # test the image if needed
 #docker run --rm -it -p 9012:9012 peregin/velocorner.rates
-docker push peregin/velocorner.rates:latest
 
 echo "Successfully deployed..."
 
